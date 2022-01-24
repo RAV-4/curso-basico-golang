609ad2acca5f8a89b3e9b003ed3c304b513272eb
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-01-24 12:01:02 -0500

    Segunda clase: constantes, variables y zero values

--- src/main.go
+++ src/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hola Mundo")
+
+	//Declaracion de constantes
+	const pi float64 = 3.1415
+	const pi2 = 3.14
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaracion de variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero value
+	var a int //Valor por defecto 0
+	var b float64 //Valor por defecto 0
+	var c string //Valor por defecto vacio
+	var d bool //Valor por defecto false
+
+	fmt.Println(a, b, c, d)
+
+	//Calcular le area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println("Area cuadrado: ", areaCuadrado)
 }
