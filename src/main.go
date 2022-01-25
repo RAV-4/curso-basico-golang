190abee97f01c56c7f61e2960aa180bb5469b387
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-01-24 19:48:58 -0500

    Paquete fmt

--- src/main.go
+++ src/main.go
@@ -96,4 +96,29 @@ func main() {
 	var areaCirculo float64 = pi * pi * radioCiculo
 	fmt.Println("Area circulo:", areaCirculo)
 
+	fmt.Println("")
+	//Paquete fmt
+	fmt.Println("Paquete fmt")
+	//Declaracion de variables
+	helloMenssage := "Hello"
+	worldMenssage := "world"
+
+	//Println
+	fmt.Println(helloMenssage, worldMenssage)
+	fmt.Println("")
+
+	//Printf
+	nombre := "Platzy"
+	cursos := 500
+	fmt.Printf("%s tiene mas de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene mas de %v cursos\n", nombre, cursos)
+	fmt.Println("")
+
+	message := fmt.Sprintf("%s tiene mas de %d cursos\n", nombre, cursos)
+	fmt.Print(message)
+
+	fmt.Println("")
+	fmt.Printf("Tipo de dato helloMessage: %T\n", helloMenssage)
+	fmt.Printf("Tipo de dato curso: %T\n", cursos)
+
 }
