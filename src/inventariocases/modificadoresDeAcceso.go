3e21efe990484f6b699ecc5f152b9b95f1127693
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-01-26 19:30:44 -0500

    Modificadores de acceso

---
+++ src/inventariocases/modificadoresDeAcceso.go
@@ -0,0 +1,20 @@
+package inventariocases
+
+import "fmt"
+
+//Las clases publicas deben tener mayuscula inicial
+type CarPublic struct {
+	Brand string //Un atributo publico tiene mayuscula inicial
+	year int //Un atributo privado tiene minuscula inicial
+}
+
+//Calse privada
+type carPrivate struct {
+	brand string
+	year int
+}
+
+//Las funciones publicas deben tener mayuscula inicial
+func PrintMessage(){
+	fmt.Println("Hola desde una funcion publica")
+}
