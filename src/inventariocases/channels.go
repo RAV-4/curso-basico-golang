4d70e944935caec3bdb58e22b88a0600152a443d
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-01-26 21:18:40 -0500

    Channels

---
+++ src/inventariocases/channels.go
@@ -0,0 +1,19 @@
+package inventariocases
+
+import "fmt"
+
+//chan<- canal de entrada de datos
+//<-chan canal de salida de datos
+func sayT(text string, c chan<- string) {
+	c <- text
+}
+
+func usoChannels() {
+	c := make(chan string, 1)
+
+	fmt.Println("Hello")
+
+	go sayT("Bye", c)
+
+	fmt.Println(<- c)
+}
